7183315dd28926d3a95634a729ee3663f62e448f
gilbert_marquez <gilbert.marquez@ucr.ac.cr>
2022-09-21 22:50:17 -0600

    Added base for Entregable_4

---
+++ Tarea 1/Entregable_4/README.md
@@ -0,0 +1,78 @@
+# Tarea Programada 1: Entregable 3
+
+## **Problema a resolver**
+
+Construir una aplicación que muestre información de avistamientos de OVNI'S, tomados desde un archivo de entrada tipo XML. En este archivo se muestran una lista de avistamientos divididos en evento, fecha, hora, descripción, país, entre otros datos. Para la resolución de este problema es necesario un analizador léxico y sintáctico que convierta este archivo en datos de entrada válidos para la aplicación a implementar.
+
+## **Descripción del archivo XML base**
+
+El archivo consta de tres partes divididas claramente:
+
+1. Lista de estados: Consiste en una lista de los estados o lugares donde ha habido al menos un reporte de avistamiento de OVNI.
+
+2. Lista de formas: Consiste en una lista de las formas de los OVNIS que han sido reportadas.
+
+3. Lista de eventos: Es la lista más larga y compleja. Consiste en una lista de cada uno de los eventos (avistamientos) que han sido reportados. Se divide en diversos tipos de datos del reporte como lugar, hora, entre otros. Además, provee un link donde se puede ver la descripción completa del suceso.
+
+Cada una de las etiquetas que están en el archivo XML tienen una etiqueta de apertura **_<ejemplo\>_** y una etiqueta de cierre **_</ejemplo\>_**.
+
+## **Descripción de la solución**
+
+La solución consta de dos analizadores: uno léxico y otro sintáctico. El analizador léxico recibe como entrada el archivo XML y se encarga de leerlo línea por línea mientras va separándolo en tokens que ya fueron especificados en el programa, mediante expresiones regulares establecidas. Además, genera una salida la cual es la entrada del analizador sintáctico correspondiente. Este analizador sintáctico es el encargado de revisar cada uno de los tokens y darse cuenta de si cumplen o no las reglas que de igual forma se establecieron en el mismo programa. 
+
+Esta solución se divide en 5 etapas, en las cuales se van agregando funcionalidades escaladamente. Para esta tercera etapa, se cuenta con archivo en código python, llamado ***lexical_analyzer.py*** en el que se cuenta con el siguiente formato y funcionalidades:
+
++ A) Formato:
+  + Lista de tokens: Una lista de cada uno de los tokens que se van a desprender de las etiquetas y de los datos contenidos en el archivo XML. 
+
+    ![Tokens_list](Images/Tokens_list.png)
+
+  + Expresiones regulares simples: Son aquellas expresiones regulares que no pasan de una línea de extensión, las cuales analizan cada una de las etiquetas del archivo XML.
+
+    ![Simple_regular_expressions](Images/Simple_regular_expressions.png)
+
+  + Expresiones regulares complejas: Son expresiones regulares que tienen una densidad de análisis más amplia, usadas para extraer los datos contenidos en el interior de cada etiqueta de apertura y de cierre.
+
+    ![Complex_regular_expressions](Images/Complex_regular_expressions.png)
+
+  + Lectura del archivo XML: Esta parte consta de una función para la apertura del archivo XML y su debida lectura línea por línea para su posterior análisis.
+
+    ![XML_Reading](Images/XML_Reading.png)
+
+  + Separar y generar la salida: Se separa cada etiqueta y su contenido en los tokens respectivos y se crea un archivo de salida de texto con el respectivo contenido de cada uno de ellos.
+
+    ![Tokenizing&Printing](Images/Tokenizing&Printing.png)
+
++ B) Funcionalidad:
+
+  Lo que el programa es capaz de realizar incluye la lectura del archivo XML, la separación de su contenido en los tokens correspondientes, la creación de un archivo de salida y en análisis de los tokens correspondientes.
+
+  En el caso del análisis sintáctico, lo que el programa en este momento es capaz de hacer incluye el respectivo análisis de la estructura de los tokens, donde se van subdividiendo en partes más simples y triviales, en donde la solución de esa parte sea sencilla y rápida. En este análisis se pueden identificar dos casos:
+
+  + Caso recursivo: Se da cuando una de las partes del código que se está analizando se llama recursivamente a sí mismo para seguir analizando sus partes. Un ejemplo sería el siguiente:  
+    
+    `stateslist_element : STATE_OPEN STATE STATE_CLOSE stateslist_element`  
+
+    En este caso, el *stateslist_element* se llama recursivamente después de analizar el token de *STATE_CLOSE*, para verificar si hay más datos que analizar con una estructura similar a esa.
+
+  + Caso no recursivo: Es el más simple de ambos, únicamente analiza la parte de código que le corresponde y finaliza esa parte de análisis porque terminó el mismo. Por ejemplo:          
+
+    `time_element : TIME_OPEN TIME TIME_CLOSE`
+
+    En este otro caso, al analizar las etiquetas correspondientes y leer el token *TIME_CLOSE*, finaliza el análisis de su parte.
+## **Ejecución del programa**
+
+Para poder ejecutar el programa y ver su salida, es necesario tener alguna versión de python instalada en el sistema operativo en el cual va a hacer la ejecución. Además, es necesario hacer la respectiva inclusión de la biblioteca _ply_ de python, la cual contiene los analizadores léxico (lexer) y sintáctico a utilizar.
+
+Para la ejecución del programa, hay que ubicarse en la carpeta que contiene el archivo _.py_ y simplemente hay que escribir el siguiente comando:
+
+`py lexical_analyzer.py`
+
+O, su equivalente:
+
+`python lexical_analyzer.py`
+
+Cabe recalcar que para las pruebas del correcto funcionamiento del programa, existe un segundo archivo _.xml_ llamado ***UFO***, el cual tiene una parte pequeña del archivo de análisis original, esto con el fin de hacer más legible y entendible el análisis correspondiente. Además, el análisis de este archivo genera su respectivo archivo de salida llamado ***syntax.txt***
+### **Archivo de salida**
+
+Al efectuar la ejecución del programa detallada anteriormente, se va a generar un archivo de salida _.txt_ el cual va a mostrar una noción de cómo se está aplicando el análisis sintáctico de los tokens.
---
+++ Tarea 1/Entregable_4/lexical_analyzer.py
@@ -0,0 +1,269 @@
+# ------------------------------------------------------------
+ # lexical_analyzer.py
+ # Authors: Daniel López, Erick Chicas, Gilbert Márquez
+ # tokenizer for a simple expression evaluator
+ # ------------------------------------------------------------
+
+import ply.lex as lex
+
+# List of token names.   This is always required
+tokens = (
+  'COUNTRY_OPEN',
+  'COUNTRY_CLOSE',
+  'COUNTRY',
+  'LINK_OPEN',
+  'LINK_CLOSE',
+  'LINK',
+  'STATE_OPEN',
+  'STATE_CLOSE',
+  'STATE',
+  'POSTED_OPEN',
+  'POSTED_CLOSE',
+  'POSTED',
+  'SHAPELIST_OPEN',#ERICK   
+  'SHAPE_OPEN',  #ERICK
+  'SHAPE_CLOSE',#ERICK
+  'SHAPELIST_CLOSE',#ERICK   
+  'SHAPE',      #ERICK
+  'EVENT_OPEN',  #ERICK
+  'EVENT_CLOSE',#ERICK
+  'CITY_OPEN',   #ERICK
+  'CITY_CLOSE', #ERICK
+  'CITY',       #ERICK
+  'SUMMARY_OPEN', #ERICK
+  'SUMMARY_CLOSE',#ERICK
+  'SUMMARY', #ERICK
+  'DURATION_OPEN', #ERICK
+  'DURATION_CLOSE',#ERICK
+  'DURATION', #ERICK
+  'STATESLIST_OPEN',
+  'STATESLIST_CLOSE',
+  'DATE_OPEN',
+  'DATE_CLOSE',
+  'DATE',
+  'IMAGES_OPEN',
+  'IMAGES_CLOSE',
+  'IMAGES',
+  'TIME_OPEN',
+  'TIME_CLOSE',
+  'TIME',
+)
+
+# Regular expression rules for simple tokens
+t_COUNTRY_OPEN = r'<country>'
+t_COUNTRY_CLOSE = r'</country>'
+t_LINK_OPEN = r'<link>'
+t_LINK_CLOSE = r'</link>'
+t_STATE_OPEN = r'<state>'
+t_STATE_CLOSE = r'</state>'
+t_SHAPELIST_OPEN = r'<shape_list>'#ERICK 
+t_SHAPE_OPEN = r'<shape>'#ERICK 
+t_SHAPE_CLOSE = r'</shape>' #ERICK
+t_SHAPELIST_CLOSE= r'</shape_list>'#ERICK 
+t_EVENT_OPEN = r'<event>' #ERICK
+t_EVENT_CLOSE = r'</event>' #ERICK
+t_CITY_OPEN = r'<city>' #ERICK
+t_CITY_CLOSE = r'</city>' #ERICK
+t_SUMMARY_OPEN = r'<summary>' #ERICK
+t_SUMMARY_CLOSE = r'</summary>' #ERICK
+t_DURATION_OPEN = r'<duration>' #ERICK
+t_DURATION_CLOSE = r'</duration>' #ERICK
+t_STATESLIST_OPEN = r'<states_list>'
+t_STATESLIST_CLOSE = r'</states_list>'
+t_DATE_OPEN = r'<date>'
+t_DATE_CLOSE = r'</date>'
+t_IMAGES_OPEN = r'<images>'
+t_IMAGES_CLOSE = r'</images>'
+t_POSTED_OPEN = r'<posted>'
+t_POSTED_CLOSE = r'</posted>'
+t_TIME_OPEN = r'<time>'
+t_TIME_CLOSE = r'</time>'
+
+# A regular expression rule with some action code
+
+def t_SUMMARY(t):
+  r'(?<=<summary>).[^<]*'
+  return t
+
+def t_COUNTRY(t):
+  r'(?<=<country>).[^<]+'
+  return t
+
+def t_STATE(t):
+  r'(?<=<state>).[^<]+'
+  return t
+
+def t_SHAPE(t):
+  r'(?<=<shape>).[^<]+'
+  return t   
+
+def t_CITY(t):
+  r'(?<=<city>)[^<]+'
+  return t 
+
+def t_DURATION(t):
+  r'(?<=<duration>).[^<]*'
+  return t          
+
+def t_DATE(t):
+  r'(?<=<date>).[^<]*'
+  return t
+
+def t_TIME(t):
+  r'(Unknown|\d{2}:\d{2})'
+  return t
+
+def t_LINK(t):
+  r'https://[\w\d@:%\._\\+#?&//=]{2,256}\.html'
+  return t
+
+def t_POSTED(t):
+  r'(\s)*(\t)*\d{1,2}\/\d{1,2}\/\d{1,2}(\s)*(\t)*'
+  return t
+
+def t_IMAGES(t):
+  r'(Yes|No)'
+  return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+  r'\n+'
+  t.lexer.lineno += len(t.value)
+ 
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+ 
+# Error handling rule
+def t_error(t):
+  f.write("Illegal character '%s'" % t.value[0])
+  print("Illegal character '%s'" % t.value[0])
+  t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+file_syntax = open("syntax.txt", "a")
+
+def p_file(t):
+  '''file : states_list shapes_list events_list'''
+  file_syntax.write("File\n")
+
+def p_events_list(t):
+  '''events_list : event'''
+  file_syntax.write("Events_list\n")
+
+def p_event(t):
+  '''event : EVENT_OPEN link_element date_element time_element city_element state_element country_element \
+  shape_element duration_element summary_element posted_element images_element EVENT_CLOSE event
+  | EVENT_OPEN link_element date_element time_element city_element state_element country_element \
+  shape_element duration_element summary_element posted_element images_element EVENT_CLOSE'''
+
+def p_states_list(t):
+  '''states_list : STATESLIST_OPEN stateslist_element STATESLIST_CLOSE'''
+  file_syntax.write(str(t[1]) + "\n" + t[3] + "\n\n")
+
+def p_stateslist_element(t):
+  '''stateslist_element : STATE_OPEN STATE STATE_CLOSE stateslist_element 
+  | STATE_OPEN STATE STATE_CLOSE'''
+  file_syntax.write(str(t[1]) + "\n" + "\t" + str(t[2]) + "\n" + t[3] + "\n")
+
+def p_shapes_list(t):
+  '''shapes_list : SHAPELIST_OPEN shapeslist_element SHAPELIST_CLOSE'''
+  file_syntax.write(str(t[1]) + "\n" + t[3] + "\n\n")     
+
+def p_shapeslist_element(t):  
+  '''shapeslist_element : SHAPE_OPEN SHAPE SHAPE_CLOSE shapeslist_element
+                       | SHAPE_OPEN SHAPE SHAPE_CLOSE  '''   
+  file_syntax.write(str(t[1]) + "\n" + "\t" + str(t[2]) + "\n" + t[3] + "\n")
+
+def p_time_element(t):
+  '''time_element : TIME_OPEN TIME TIME_CLOSE 
+                  | TIME_OPEN TIME_CLOSE'''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_country_element(t):
+  '''country_element : COUNTRY_OPEN COUNTRY COUNTRY_CLOSE 
+                      | COUNTRY_OPEN COUNTRY_CLOSE'''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_summary_element(t):
+  '''summary_element : SUMMARY_OPEN SUMMARY SUMMARY_CLOSE 
+                      | SUMMARY_OPEN SUMMARY_CLOSE'''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_posted_element(t):
+  '''posted_element : POSTED_OPEN POSTED POSTED_CLOSE
+                    | POSTED_OPEN POSTED_CLOSE'''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_duration_element(t):
+  '''duration_element : DURATION_OPEN DURATION DURATION_CLOSE
+                      | DURATION_OPEN DURATION_CLOSE'''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_state_element(t):
+  '''state_element : STATE_OPEN STATE STATE_CLOSE
+                    | STATE_OPEN STATE_CLOSE'''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_date_element(t):
+  '''date_element : DATE_OPEN DATE DATE_CLOSE
+                  | DATE_OPEN DATE_CLOSE'''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_link_element(t):
+  '''link_element : LINK_OPEN LINK LINK_CLOSE
+                  | LINK_OPEN LINK_CLOSE'''
+  file_syntax.write("<event>\n")
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_images_element(t):
+  '''images_element : IMAGES_OPEN IMAGES IMAGES_CLOSE
+                    | IMAGES_OPEN IMAGES_CLOSE'''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+  file_syntax.write("</event>\n\n")
+
+def p_city_element(t):
+  '''city_element  : CITY_OPEN CITY CITY_CLOSE 
+                   | CITY_OPEN  CITY_CLOSE  '''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_shape_element(t):
+  '''shape_element : SHAPE_OPEN  SHAPE SHAPE_CLOSE shape_element 
+                  | SHAPE_OPEN SHAPE SHAPE_CLOSE  '''
+  file_syntax.write("\t" + str(t[1]) + "\n" + "\t\t" + str(t[2]) + "\n" + "\t" + t[3] + "\n")
+
+def p_error(t):
+    print("Syntax error at '%s'" % t[0].value)
+
+# Read the file
+lines = []
+def open_file(filename):
+  data = open(filename, "r", encoding="utf-8")
+  for line in data.readlines():
+    lines.append(line.replace('\n', ''))
+  with open (filename, "r") as filecontents:
+    data = filecontents.read().replace('\n', ' ')
+  return data
+
+#data = open_file("UFO_Report_2022.xml")
+data = open_file("UFO.xml")
+
+f = open("results.txt", "a")
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+  tok = lexer.token()
+  if not tok: 
+    break      # No more input
+  f.write(str(tok.value)+" | " +  str(tok.type) + "\n")
+  #print(tok)
+
+import ply.yacc as yacc
+parser = yacc.yacc()
+parser.parse(data)
+
+f.close()
