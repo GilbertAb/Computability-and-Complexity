cb3285d641ed982307fedf795cb352dadb0605bc
gilbert_marquez <gilbert.marquez@ucr.ac.cr>
2022-11-29 13:37:54 -0600

    Added mutation function and method to run the genetic algorithm

--- Tarea_2/sudoku_ga.py
+++ Tarea_2/sudoku_ga.py
@@ -2,7 +2,6 @@ from sudoku import Sudoku
 import random
 import copy
 
-
 BOARD_SIZE = 9
 SUBGRID_SIZE = 3
 
@@ -24,7 +23,63 @@ class SudokuGA:
         population = self.create_population(sudoku)
         ranked_population = self.fitness(population)
         next_breeders = self.selection(ranked_population)
-        self.crossover(next_breeders)
+        new_generation = self.crossover(next_breeders)
+        new_generation = self.mutate(new_generation)
+    def run(self, sudoku):
+        """
+        Start the GA to solve the objects
+        """
+        #values_to_set = self._load().get_initial_values()
+
+        best_data = []
+        worst_data = []
+        found = False
+        overall_num_generations_done = 0
+        restart_counter = 0
+        population=[]
+        while overall_num_generations_done < self.max_num_generations and not found:
+            population = self.create_population(sudoku)
+            print("Population:", len(population))
+            num_generations_done = 0
+            remember_the_best = 0
+            num_generations_without_improvement = 0
+
+            # Loop until max allowed generations is reached or a solution is found
+            while num_generations_done < self.max_num_generations and not found:
+                print(num_generations_done)
+                # Rank the solutions
+                ranked_population = self.fitness(population)
+                best_solution = ranked_population[0]
+                best_score = best_solution.count_duplicates()
+
+                # Check if problem is solved and print best and worst results
+                if best_score > 0:
+                    next_breeders = self.selection(ranked_population)
+                    new_generation = self.crossover(next_breeders)
+                    population = self.mutate(new_generation)
+                    num_generations_done += 1
+                    overall_num_generations_done += 1
+                else:
+                    print("Problem solved after {} generations ({} overall generations)!!! Solution found is:".
+                          format(num_generations_done, overall_num_generations_done))
+                    best_solution.print_solution()
+                    found = True
+
+        if not found:
+            print("Problem not solved after {} generations. Printing best and worst results below".
+                  format(overall_num_generations_done))
+            #ranked_population = ga_utils.rank_population(new_population)
+            ranked_population = self.fitness(population)
+            best_solution = ranked_population[0]
+            worst_solution = ranked_population[-1]
+            print("Best is:")
+            print(best_solution.count_duplicates())
+            best_solution.print_solution()
+            print("Worst is:")
+            worst_solution.print_solution()
+
+        #graphics.draw_best_worst_fitness_scores(best_data, worst_data)
+
     # Create population
     def create_population(self, sudoku):
         population = []
@@ -36,6 +91,8 @@ class SudokuGA:
         individual = Sudoku(sudoku)
         sudoku_solution = individual.solution
         # TODO: avoid duplicated values in 3x3 grid
+        #values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
         for row in range(BOARD_SIZE):
             for column in range(BOARD_SIZE):
                 random_val = random.randrange(1, 10)
@@ -95,7 +152,14 @@ class SudokuGA:
             child.set_3x3_subgrid(mother_grids[i], i)
         return child
     # Mutation
- 
+    def mutate(self, next_generation):
+        mutated_population = []
+        for individual in next_generation:
+            if random.random() < self.mutation_rate:
+                individual.swap_2_values(random.randrange(0, BOARD_SIZE))
+            mutated_population.append(individual)
+        return mutated_population
+
 sudo = [
         [8,6,0,2,4,7,5,0,0],
         [7,0,9,1,5,8,6,0,2],
@@ -109,9 +173,9 @@ sudo = [
 
 #pop = create_population(sudo, 5)
 ag = SudokuGA(population_size=20, selection_rate=0.25, random_selection_rate=0.25,
-            num_children=4, max_num_generations=0, mutation_rate=0.25, model_to_solve=0,
-            presolving=0, restart_after_n_generations_without_improvement=0)
-ag.solve(sudo)
+            num_children=4, max_num_generations=2000, mutation_rate=0.25, model_to_solve=0,
+            presolving=0, restart_after_n_generations_without_improvement=20)
+ag.run(sudo)
 # Print population
 #for i in pop:
 #    i.print_solution()
